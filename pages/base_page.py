1744683e3625ebf5188fc6dc2dba94104376a07a
Alexandr <volokitin200@bk.ru>
2025-08-19 16:08:52 +0300

    feat: added login page element presence checkers

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,4 +1,4 @@
-from selenium import webdriver
+from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
@@ -6,7 +6,7 @@ from selenium.webdriver.support import expected_conditions as EC
 
 
 class BasePage():
-    def __init__(self, browser: webdriver, url: str, timeout:5) -> None:
+    def __init__(self, browser: WebDriver, url: str, timeout=5) -> None:
         self.browser = browser
         self.url = url
         self.timeout = timeout
@@ -15,7 +15,7 @@ class BasePage():
     def open(self) -> None:
         self.browser.get(self.url)
     
-    def is_element_present(self, searching_method: By, target: str) -> bool:
+    def is_element_present(self, searching_method: str, target: str) -> bool:
         try:
             WebDriverWait(self.browser, self.timeout).until(EC.presence_of_element_located((searching_method, target)))
         except TimeoutException:
